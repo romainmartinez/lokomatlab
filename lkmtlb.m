cb1dc3275cbf69b318752c364fe3b1beddac05d0
Romain Martinez <martinez.staps@gmail.com>
2017-08-31 09:08:27 -0400

    setup git

---
+++ lkmtlb.m
@@ -0,0 +1,15 @@
+%{
+    %   Description: Lokomat analysis
+    %
+    %   author:  Romain Martinez
+    %   email:   martinez.staps@gmail.com
+    %   website: github.com/romainmartinez
+%}
+clear variables; clc; close all
+
+lkmtlb.main;
+
+% todo:
+% Hotelling test
+% automate orgData (spm & gramm friendly)
+% gramm to plot spm (not R)
